047fbebd47c9fa99838f0f4155fb86aa0fc081cb
David Pazmiño <dpazmino@webcreek.com>
2019-08-04 21:24:19 -0500

    DP-000 | Creation of the app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
