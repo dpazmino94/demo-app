3f3af5cc63bfb987bae01bc987e510ad876a854b
David Pazmiño <dpazmino@webcreek.com>
2019-08-05 16:56:20 -0500

    FCMA-000 | Adding Styles to the to do list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
-
+import { CommonDialogComponent } from './common/common-dialog/common-dialog.component'
 // Angular Material imports
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-//import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    CommonDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTreeModule
   ],
-  providers: [],
+  entryComponents: [CommonDialogComponent],
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: true}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
